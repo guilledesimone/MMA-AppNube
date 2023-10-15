d156afe741eac616bb00ba50b61d7fae826025bf
guilledesimone <104263162+guilledesimone@users.noreply.github.com>
2023-10-14 23:13:05 -0300

    Delete models-in-production directory

--- models-in-production/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sh clean.sh
-zip -r models-in-production.zip app.py predictions.py aws_utils.py config.py requirements.txt dist
--- models-in-production/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf .ipynb_checkpoints
-rm dist/*
-rm models-in-production.zip
-rm -rf env
